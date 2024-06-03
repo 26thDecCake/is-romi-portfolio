b542eddea029459dcd6267e455058604e2b544a8
Romi Than <rrromithan@gmail.com>
2024-06-03 17:10:33 +0700

    feat: merge

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  // base: "/is-romi-portfolio",
+  base: "/is-romi-portfolio",
   plugins: [react()],
 });
